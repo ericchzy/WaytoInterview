d3a6242ad67f3697fe3657fada1d26e1aa176436
cdee <zycheng.gm@gmail.com>
2018-08-10 10:41:21 +0800

    Add ListMerge.

---
+++ src/com/company/io.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class io {
+    /* This funtcion is here just to test buildTree() */
+    public static void printInorder(TreeNode node)
+    {
+        if (node == null)
+            return;
+
+        /* first recur on left child */
+        printInorder(node.left);
+
+        /* then print the data of node */
+        System.out.print(node.val + " ");
+
+        /* now recur on right child */
+        printInorder(node.right);
+    }
+
+    /* Given a binary tree, print its nodes according to the
+    "bottom-up" postorder traversal. */
+    public static void printPostorder(TreeNode node)
+    {
+        if (node == null)
+            return;
+
+        // first recur on left subtree
+        printPostorder(node.left);
+
+        // then recur on right subtree
+        printPostorder(node.right);
+
+        // now deal with the node
+        System.out.print(node.val + " ");
+    }
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.value + "->");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+}
