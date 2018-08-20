2a3ca41774844e70e987aed9903f10381b618339
cdee <zycheng.gm@gmail.com>
2018-08-20 12:08:11 +0800

    Add ColorBrick

---
+++ src/com/company/Netease/ColorBrick.java
@@ -0,0 +1,23 @@
+package com.company.Netease;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class ColorBrick {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        HashSet<String> colorSet = new HashSet<String>();
+        for (int i = 0; i < string.length(); i++) {
+            colorSet.add(string.substring(i, i + 1));
+
+            if (colorSet.size() > 2) {
+                System.out.println("0");
+                return;
+            }
+        }
+        System.out.println(colorSet.size());
+
+    }
+}
