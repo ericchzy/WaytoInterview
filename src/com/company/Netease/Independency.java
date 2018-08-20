a2014a2465b16abecea91dd9aba3c31bc5277e17
cdee <zycheng.gm@gmail.com>
2018-08-20 10:08:04 +0800

    Add Netease Independency ver.1

--- src/com/company/Netease/Independency.java
+++ src/com/company/Netease/Independency.java
@@ -3,6 +3,7 @@ package com.company.Netease;
 /**
  * Version 1
  * case通过率为60.00%, 未通过的测试例子 2 17 20 1 对应输出 10， 错误输出 12
+ * 错误分析： 代码实际上是通过增加可以卖出手中的苹果这一条件来实现，但是题目条件并没有给出
  */
 
 import java.util.Scanner;
