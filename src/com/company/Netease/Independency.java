756e17c5a1b625f41c7429de756c389e3eed5756
cdee <zycheng.gm@gmail.com>
2018-08-20 10:25:37 +0800

    Add Netease Independency ver.3

--- src/com/company/Netease/Independency.java
+++ src/com/company/Netease/Independency.java
@@ -4,6 +4,11 @@ package com.company.Netease;
  * Version 1
  * case通过率为60.00%, 未通过的测试例子 2 17 20 1 对应输出 10， 错误输出 12
  * 错误分析： 代码实际上是通过增加可以卖出手中的苹果这一条件来实现，但是题目条件并没有给出
+ * Version 2
+ * case通过率为70.00%, 未通过的测试例子 1 1996245611 1999990159 123 对应输出 1996275808， 错误输出 -12660506
+ * 错误分析：int TotalBalance 数值溢出
+ * Version 3
+ * 正确
  */
 
 import java.util.Scanner;
@@ -18,10 +23,15 @@ public class Independency {
         int Balance = scanner.nextInt();
         int ApplePrice = scanner.nextInt();
 
-        int TotalBalance = Balance + AppleNum * ApplePrice;
-        int TotalCostPerDay = rentPrice + CostApplePerDay * ApplePrice;
+        /* Deal with the shortage of the Balance */
+        if (AppleNum >= (Balance / rentPrice)) {
+            System.out.println((Balance / rentPrice));
+        } else {
+            /* Deal with the shortage of the Apple */
+            System.out.println(AppleNum + (Balance - AppleNum * rentPrice) / (ApplePrice + rentPrice));
+        }
 
-        System.out.println(TotalBalance / TotalCostPerDay);
+        scanner.close();
 
     }
 }
