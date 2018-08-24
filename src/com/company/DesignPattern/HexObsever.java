c4636b94a51c6045826031774d6ac8bb8d639181
cdee <zycheng.gm@gmail.com>
2018-08-24 12:41:25 +0800

    Add Observer Design Pattern.

---
+++ src/com/company/DesignPattern/HexObsever.java
@@ -0,0 +1,13 @@
+package com.company.DesignPattern;
+
+public class HexObsever extends Observer {
+    public HexObsever(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Hex String: " + Integer.toHexString(subject.getState()).toUpperCase());
+    }
+}
