a217646f040904a04743cdf16047b7d1d38bbefa
dee <czyxyz@foxmail.com>
2018-09-30 10:12:58 +0800

    Code Cleanup.

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -3,6 +3,10 @@ package com.company;
 //import com.company.util.TestUtil;
 //import com.company.TestUtil;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Test {
     public static void main(String[] args)  {
 //        SimilarString str=null;
@@ -89,6 +93,75 @@ public class Test {
         System.out.println("-----");
         System.out.println(14^3);
 
+
+        /**
+         * HashMap遍历效率技巧：
+         * 1> 通过keySet来遍历效率低下，对于keySet其实是遍历了2次，一次是转为iterator，一次就是从HashMap中取出key所对于的value
+         * 2> 通过entrySet来遍历效率较高
+         */
+
+
+        System.out.println("-----");
+        Map map = new HashMap();
+        map.put("key1","lisi1");
+        map.put("key2","lisi2");
+        map.put("key3","lisi3");
+        map.put("key4","lisi4");
+
+        // 使用keySet遍历
+        Iterator keyIterator = map.keySet().iterator();
+        while (keyIterator.hasNext()) {
+            Object key = keyIterator.next();
+            System.out.println(map.get(key));
+        }
+        //将map集合中的映射关系取出，存入到set集合
+        Iterator it = map.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry e =(Map.Entry) it.next();
+            System.out.println("键"+e.getKey () + "的值为" + e.getValue());
+        }
+
+        /**
+         * 自定义类型作为Map的键
+         */
+        System.out.println("-----");
+        HashMap<Person, String> personMap = new HashMap<>();
+
+        personMap.put(new Person("001"), "findingsea");
+        personMap.put(new Person("002"), "linyin");
+        personMap.put(new Person("003"), "henrylin");
+        personMap.put(new Person("003"), "findingsealy");
+
+        System.out.println(personMap.toString());
+
+        System.out.println(personMap.get(new Person("001")));
+        System.out.println(personMap.get(new Person("002")));
+        System.out.println(personMap.get(new Person("003")));
+
+
+        /**
+         * String.hashCode()
+         */
+
+        System.out.println("findingsea".hashCode());
+        System.out.println("findingsea".hashCode());
+        System.out.println(new String("findingsea").hashCode());
+        System.out.println(new String("findingsea").hashCode());
+
+        /**
+         * thread.run()还是当作普通方法的方式调用，程序还是要顺序执行，要等待run方法体执行完毕后，才可继续执行下面的代码
+         * thread.start()才是将线程体放入到线程等待序列之中等待线程调度程序运行
+         */
+        System.out.println("-----");
+        Runner1 runner1 = new Runner1();
+        Runner2 runner2 = new Runner2();
+//		Thread(Runnable target) 分配新的 Thread 对象。
+        Thread thread1 = new Thread(runner1);
+        Thread thread2 = new Thread(runner2);
+//		thread1.start();
+//		thread2.start();
+        thread1.run();
+        thread2.run();
     }
 
     public static int test() {
@@ -112,4 +185,47 @@ public class Test {
             val = -1;
         }
     }
+
+
+    static class Runner1 implements Runnable { // 实现了Runnable接口，jdk就知道这个类是一个线程
+        public void run() {
+            for (int i = 0; i < 2; i++) {
+                System.out.println("进入Runner1运行状态——————————" + i);
+            }
+        }
+    }
+
+    static class Runner2 implements Runnable { // 实现了Runnable接口，jdk就知道这个类是一个线程
+        public void run() {
+            for (int i = 0; i < 2; i++) {
+                System.out.println("进入Runner2运行状态==========" + i);
+            }
+        }
+    }
 }
+
+class Person {
+    private String id;
+
+    public Person(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != null ? !id.equals(person.id) : person.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
+
