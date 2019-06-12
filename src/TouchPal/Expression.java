ecfd3e8012107113663a8aa37fd04e71031e4e65
cdee <zycheng.gm@gmail.com>
2019-06-12 11:25:30 +0800

    Add hw package.

--- src/TouchPal/Expression.java
+++ src/TouchPal/Expression.java
@@ -7,60 +7,60 @@ public class Expression {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while (in.hasNext()) {
-            String exp=in.nextLine();
-            ArrayDeque<String> expQue=new ArrayDeque<>();
-            StringBuilder digit=new StringBuilder();
-            for(int i=0;i<exp.length();i++){
-                char ch=exp.charAt(i);
+            String exp = in.nextLine();
+            ArrayDeque<String> expQue = new ArrayDeque<>();
+            StringBuilder digit = new StringBuilder();
+            for (int i = 0; i < exp.length(); i++) {
+                char ch = exp.charAt(i);
                 //处理额外负号
-                if(digit.length()==0&&ch=='-'&&
-                        (i==0||"/".equals(expQue.peekLast()) ||"*".equals(expQue.peekLast()))){
+                if (digit.length() == 0 && ch == '-' &&
+                        (i == 0 || "/".equals(expQue.peekLast()) || "*".equals(expQue.peekLast()))) {
                     digit.append(ch);
-                } else if(ch=='+'||ch=='-'||ch=='*'||ch=='/'){
-                    if(digit.length()>0){//数字入队列
+                } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                    if (digit.length() > 0) {//数字入队列
                         expQue.add(digit.toString());
-                        digit.delete(0,digit.length());
+                        digit.delete(0, digit.length());
                     }
                     expQue.add(String.valueOf(ch));
-                } else{
+                } else {
                     digit.append(ch);
                 }
             }
-            if(digit.length()>0) expQue.add(digit.toString());
-            ArrayDeque<String> expQueTemp=new ArrayDeque<>();
-            String first,numA,numB;
+            if (digit.length() > 0) expQue.add(digit.toString());
+            ArrayDeque<String> expQueTemp = new ArrayDeque<>();
+            String first, numA, numB;
 
             //expQueTemp的最后一个数即运算符的前一个数，expQue的第一个数即运算符的后一个数
             //处理+-法
-            while(!expQue.isEmpty()){
-                first=expQue.removeFirst();
-                if("+".equals(first)){
-                    numA=expQueTemp.pollLast();
-                    numB=expQue.pollFirst();
-                    expQueTemp.add(String.valueOf(Integer.parseInt(numA)+Integer.parseInt(numB)));
-                }else if("-".equals(first)){
-                    numA=expQueTemp.pollLast();
-                    numB=expQue.pollFirst();
-                    expQueTemp.add(String.valueOf(Integer.parseInt(numA)-Integer.parseInt(numB)));
-                }else{
+            while (!expQue.isEmpty()) {
+                first = expQue.removeFirst();
+                if ("+".equals(first)) {
+                    numA = expQueTemp.pollLast();
+                    numB = expQue.pollFirst();
+                    expQueTemp.add(String.valueOf(Integer.parseInt(numA) + Integer.parseInt(numB)));
+                } else if ("-".equals(first)) {
+                    numA = expQueTemp.pollLast();
+                    numB = expQue.pollFirst();
+                    expQueTemp.add(String.valueOf(Integer.parseInt(numA) - Integer.parseInt(numB)));
+                } else {
                     expQueTemp.add(first);
                 }
             }
-            expQue=expQueTemp;
+            expQue = expQueTemp;
 
             //处理*/法
-            expQueTemp=new ArrayDeque<>();
-            while(!expQue.isEmpty()){
-                first=expQue.removeFirst();
-                if("*".equals(first)){
-                    numA=expQueTemp.pollLast();
-                    numB=expQue.pollFirst();
-                    expQueTemp.add(String.valueOf(Integer.parseInt(numA)*Integer.parseInt(numB)));
-                }else if("/".equals(first)){
-                    numA=expQueTemp.pollLast();
-                    numB=expQue.pollFirst();
-                    expQueTemp.add(String.valueOf(Integer.parseInt(numA)/Integer.parseInt(numB)));
-                }else{
+            expQueTemp = new ArrayDeque<>();
+            while (!expQue.isEmpty()) {
+                first = expQue.removeFirst();
+                if ("*".equals(first)) {
+                    numA = expQueTemp.pollLast();
+                    numB = expQue.pollFirst();
+                    expQueTemp.add(String.valueOf(Integer.parseInt(numA) * Integer.parseInt(numB)));
+                } else if ("/".equals(first)) {
+                    numA = expQueTemp.pollLast();
+                    numB = expQue.pollFirst();
+                    expQueTemp.add(String.valueOf(Integer.parseInt(numA) / Integer.parseInt(numB)));
+                } else {
                     expQueTemp.add(first);
                 }
             }
@@ -68,6 +68,7 @@ public class Expression {
         }
         in.close();
     }
+
 }
 
 
--- src/TouchPal/GodProblem.java
+++ src/TouchPal/GodProblem.java
@@ -58,48 +58,48 @@ public class GodProblem {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int height=in.nextInt();
-        int width=in.nextInt();
-        int waterLevel=in.nextInt();
+        int height = in.nextInt();
+        int width = in.nextInt();
+        int waterLevel = in.nextInt();
         int[][] map = new int[height][width];
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 map[i][j] = in.nextInt();
             }
         }
-        boolean [][]isVisited=new boolean[height][width];
+        boolean[][] isVisited = new boolean[height][width];
 
         //处理洪水从地图的四周开始涨起
-        for(int i=0;i<height;i++){
-            if(!isVisited[i][0])
-                dfs(map,isVisited,i,0,waterLevel);
-            if(!isVisited[i][width-1])
-                dfs(map,isVisited,i,width-1,waterLevel);
+        for (int i = 0; i < height; i++) {
+            if (!isVisited[i][0])
+                dfs(map, isVisited, i, 0, waterLevel);
+            if (!isVisited[i][width - 1])
+                dfs(map, isVisited, i, width - 1, waterLevel);
         }
-        for(int j=1;j<width-1;j++){
-            if(!isVisited[0][j])
-                dfs(map,isVisited,0,j,waterLevel);
-            if(!isVisited[height-1][j])
-                dfs(map,isVisited,height-1,j,waterLevel);
+        for (int j = 1; j < width - 1; j++) {
+            if (!isVisited[0][j])
+                dfs(map, isVisited, 0, j, waterLevel);
+            if (!isVisited[height - 1][j])
+                dfs(map, isVisited, height - 1, j, waterLevel);
         }
-        int countExist=0;
-        for(int i=0;i<height;i++)
-            for(int j=0;j<width;j++)
-                if(map[i][j]!=0) countExist++;
+        int countExist = 0;
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                if (map[i][j] != 0) countExist++;
         System.out.println(countExist);
         in.close();
     }
 
     //对一个位置的四个方向分别进行深度优先搜索
-    private static void dfs(int [][]map,boolean [][]isVisited,int i,int j,int waterLevel){
-        if(i<0||i>=map.length||j<0||j>=map[0].length||isVisited[i][j]) return ;
-        isVisited[i][j]=true;
-        if(waterLevel<map[i][j]) return ;
-        else map[i][j]=0;  //将其摧毁
-        dfs(map,isVisited,i+1,j,waterLevel);
-        dfs(map,isVisited,i-1,j,waterLevel);
-        dfs(map,isVisited,i,j+1,waterLevel);
-        dfs(map,isVisited,i,j-1,waterLevel);
+    private static void dfs(int[][] map, boolean[][] isVisited, int i, int j, int waterLevel) {
+        if (i < 0 || i >= map.length || j < 0 || j >= map[0].length || isVisited[i][j]) return;
+        isVisited[i][j] = true;
+        if (waterLevel < map[i][j]) return;
+        else map[i][j] = 0;  //将其摧毁
+        dfs(map, isVisited, i + 1, j, waterLevel);
+        dfs(map, isVisited, i - 1, j, waterLevel);
+        dfs(map, isVisited, i, j + 1, waterLevel);
+        dfs(map, isVisited, i, j - 1, waterLevel);
     }
 }
 
@@ -109,4 +109,29 @@ public class GodProblem {
 5 1 5
 5 1 1
 输出： 6
+
+5 5 3
+3 3 3 3 3
+3 4 3 4 3
+3 3 3 3 3
+3 4 3 4 3
+3 3 3 3 3
+out: 4
+
+5 5 3
+3 3 3 3 3
+3 4 4 4 3
+3 4 1 4 3
+3 4 4 4 3
+3 3 3 3 3
+out: 9
+
+
+5 5 3
+3 3 3 3 3
+3 4 3 4 3
+4 1 4 1 4
+3 4 3 4 3
+3 3 3 3 3
+out: 9
  *///:~
